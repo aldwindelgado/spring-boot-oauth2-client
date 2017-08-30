05d62bccd917def857736946b76a9238a9d49a24
Aldwin Delgado <aldwinpaolodelgado@gmail.com>
2017-08-30 20:20:46 +0800

    Renamed packages Updated app-name

--- src/main/java/com/github/oauth2/client/ClientApplication.java
+++ src/main/java/com/github/oauth2/client/ClientApplication.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.github.oauth.client.sample;
+package com.github.oauth2.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/github/oauth2/client/ResourceServerConfig.java
+++ src/main/java/com/github/oauth2/client/ResourceServerConfig.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.github.oauth.client.sample;
+package com.github.oauth2.client;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
